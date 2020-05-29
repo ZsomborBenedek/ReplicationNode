657b66a61f94e56aa1775b44f31768159f2998ad
Benedek Zsombor Ákos <zsomborbeni@gmail.com>
2020-05-29 19:42:03 +0200

    Hopefully working relative filepaths

--- src/main/java/com/example/demo/service/TCPListner.java
+++ src/main/java/com/example/demo/service/TCPListner.java
@@ -4,61 +4,63 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class TCPListner implements Runnable{
+public class TCPListner implements Runnable {
     RestNodeService nodeService;
     boolean mode;
     String file;
-    public TCPListner(RestNodeService nodeService, boolean mode, String file){
+
+    public TCPListner(RestNodeService nodeService, boolean mode, String file) {
         this.nodeService = nodeService;
         this.mode = mode;
         this.file = file;
     }
+
     public void sendTCP() throws IOException {
         ServerSocket s = new ServerSocket(6969);
         Socket sr = s.accept();
         FileInputStream fr = null;
-        if(nodeService.name.equals("host2"))
-            fr = new FileInputStream("/home/pi/ReplicationNode/src/localFilesHost1/"+file);
-        if(nodeService.name.equals("host3"))
-            fr = new FileInputStream("/home/pi/ReplicationNode/src/LocalFilesHost2/"+file);
-        if(nodeService.name.equals("host4"))
-            fr = new FileInputStream("/home/pi/ReplicationNode/src/LocalFilesHost3/"+file);
-        if(nodeService.name.equals("host5"))
-            fr = new FileInputStream("/home/pi/ReplicationNode/src/LocalFilesHost4/"+file);
+        if (nodeService.name.equals("host2"))
+            fr = new FileInputStream(new File("src/localFilesHost1/" + file).getAbsolutePath());
+        if (nodeService.name.equals("host3"))
+            fr = new FileInputStream(new File("src/LocalFilesHost2/" + file).getAbsolutePath());
+        if (nodeService.name.equals("host4"))
+            fr = new FileInputStream(new File("src/LocalFilesHost3/" + file).getAbsolutePath());
+        if (nodeService.name.equals("host5"))
+            fr = new FileInputStream(new File("src/LocalFilesHost4/" + file).getAbsolutePath());
 
         byte b[] = new byte[5000];
-        fr.read(b,0,b.length);
+        fr.read(b, 0, b.length);
         OutputStream os = sr.getOutputStream();
-        os.write(b,0,b.length);
-        System.out.println("Sending "+file+" completed");
+        os.write(b, 0, b.length);
+        System.out.println("Sending " + file + " completed");
         s.close();
     }
+
     public void transferTCP() throws IOException {
         ServerSocket s = new ServerSocket(6969);
         Socket sr = s.accept();
         FileInputStream fr = null;
-        //if(nodeService.name.equals("host2"))
-        fr = new FileInputStream("/home/pi/ReplicationNode/src/replicatedFiles/"+file);
+        // if(nodeService.name.equals("host2"))
+        fr = new FileInputStream(new File("src/replicatedFiles/" + file).getAbsolutePath());
         byte b[] = new byte[5000];
-        fr.read(b,0,b.length);
+        fr.read(b, 0, b.length);
         OutputStream os = sr.getOutputStream();
-        os.write(b,0,b.length);
-        File temp = new File("/home/pi/ReplicationNode/src/replicatedFiles/"+file);
+        os.write(b, 0, b.length);
+        File temp = new File(new File("src/replicatedFiles/" + file).getAbsolutePath());
         temp.delete();
-        System.out.println("Sending "+file+" completed");
+        System.out.println("Sending " + file + " completed");
         s.close();
     }
 
     @Override
     public void run() {
-        if(mode) {
+        if (mode) {
             try {
                 sendTCP();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-        else {
+        } else {
             try {
                 transferTCP();
             } catch (IOException e) {
